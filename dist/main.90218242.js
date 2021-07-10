1494050a61380feee6c019427300a726433aac5f
Kanan Mirzayev <58333179+KananMirza@users.noreply.github.com>
2021-07-10 14:23:05 +0400

    changed font weight

--- dist/main.90218242.js
+++ dist/main.90218242.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53831" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58639" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
